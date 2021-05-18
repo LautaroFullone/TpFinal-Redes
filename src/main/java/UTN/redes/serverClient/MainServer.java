ff20607dba707263154be936c95fa943aee48b27
LautaroFullone <lautarofullone@gmail.com>
2021-05-17 21:09:40 -0300

    First Commit

---
+++ src/main/java/UTN/redes/serverClient/MainServer.java
@@ -0,0 +1,21 @@
+package UTN.redes.serverClient;
+
+import UTN.redes.serverClient.models.Server;
+
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainServer
+{
+    public static void main(String[] args) throws IOException
+    {
+        Server serv = new Server();
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer();
+    }
+}
---
+++ src/main/java/UTN/redes/serverClient/models/Connect.java
@@ -0,0 +1,28 @@
+package UTN.redes.serverClient.models;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Connect {
+
+    private final int PUERTO = 3000; //Puerto para la conexión
+    private final String HOST = "localhost"; //Host para la conexión
+    protected String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor
+    protected ServerSocket ss; //Socket del servidor
+    protected Socket cs; //Socket del cliente
+    protected DataOutputStream salidaServidor;
+    protected DataOutputStream salidaCliente;
+
+    public Connect(String tipo) throws IOException //Constructor
+    {
+        if(tipo.equalsIgnoreCase("servidor")) {
+            ss = new ServerSocket(PUERTO);
+            cs = new Socket();
+        }
+        else
+            cs = new Socket(HOST, PUERTO);
+
+    }
+}
