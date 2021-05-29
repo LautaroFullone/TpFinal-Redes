7b55b27e056be3febed710f775a2e5092a74de23
LautaroFullone <lautarofullone@gmail.com>
2021-05-28 21:00:22 -0300

    Update MainClient.java

--- src/main/java/UTN/redes/serverClient/MainClient.java
+++ src/main/java/UTN/redes/serverClient/MainClient.java
@@ -1,13 +1,11 @@
 package UTN.redes.serverClient;
 
 import UTN.redes.serverClient.models.MyClient;
-import com.sun.security.ntlm.Client;
-import com.sun.security.ntlm.NTLMException;
 
 import java.io.IOException;
 
 public class MainClient {
-    public static void main(String[] args) throws IOException, NTLMException {
+    public static void main(String[] args) throws IOException {
         MyClient c = new MyClient(); //Se crea el cliente
 
         System.out.println("Iniciando cliente\n");
